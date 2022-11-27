dc7a7bc65ebef8d7a96d456e9ec0819dc2041a6a
Milan-Sharma-449 <milansharma449@gmail.com>
2022-11-27 14:49:05 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/app'
+
+const config = {
+    apiKey: "AIzaSyB6_Z_Nq1UO6laMt4-VyWHwYTU1szeA0P8",
+    authDomain: "chat-web-app-d4c52.firebaseapp.com",
+    projectId: "chat-web-app-d4c52",
+    storageBucket: "chat-web-app-d4c52.appspot.com",
+    messagingSenderId: "414075034452",
+    appId: "1:414075034452:web:9565ea2105abbf682086c1"
+  };
