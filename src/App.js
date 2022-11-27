343e4f29488ecd21c81a3b8ff1175861f645392e
Milan-Sharma-449 <milansharma449@gmail.com>
2022-11-27 13:16:29 +0530

    scafloding chat app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// eslint-disable-next-line import/no-unresolved
+import 'rsuite/dist/rsuite.min.css'
+import './styles/main.scss'
+
+
+function App() {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+}
+
+export default App;
