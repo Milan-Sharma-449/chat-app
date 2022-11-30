a2ef83f83cf56c55294dcb4ef73435390f96992e
Milan-Sharma-449 <milansharma449@gmail.com>
2022-11-30 16:31:59 +0530

    added signout btn and display username

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Col, Grid, Row } from 'rsuite'
+import Sidebar from '../Components/Sidebar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Grid fluid className='h-100'>
+        <Row>
+            <Col xs={24} md={8}>
+                <Sidebar />
+            </Col>
+        </Row>
+    </Grid>
   )
 }
 
